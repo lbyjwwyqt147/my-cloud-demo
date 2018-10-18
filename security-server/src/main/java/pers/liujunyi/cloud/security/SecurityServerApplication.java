2d59bab1c421d7c4d683e74f2ce7aa2e620d00c5
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2018-10-18 17:10:15 +0800

    初始化工程

---
+++ security-server/src/main/java/pers/liujunyi/cloud/security/SecurityServerApplication.java
@@ -0,0 +1,12 @@
+package pers.liujunyi.cloud.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityServerApplication.class, args);
+    }
+}
